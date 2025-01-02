2d000f316ef32903b04a724dad211bfdc909faa3
kosuke222naito <kosuke.naito.engineer@gmail.com>
2025-01-02 11:40:00 +0900

    expand RendererOptions interface

--- packages/runtime-core/renderer.ts
+++ packages/runtime-core/renderer.ts
@@ -1,5 +1,11 @@
 export interface RendererOptions<HostNode = RendererNode> {
+  createElement(type: string): HostNode;
+
+  createText(text: string): HostNode;
+
   setElementText(node: HostNode, text: string): void;
+
+  insert(child: HostNode, parent: HostNode, anchor?: HostNode | null) :void;
 }
 
 export interface RendererNode {
