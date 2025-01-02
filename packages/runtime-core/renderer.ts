010ddf7d373928a1cfac893088179524919172b9
kosuke222naito <kosuke.naito.engineer@gmail.com>
2025-01-02 11:46:04 +0900

    implement renderVnode

--- packages/runtime-core/renderer.ts
+++ packages/runtime-core/renderer.ts
@@ -1,3 +1,5 @@
+import { VNode } from "./vnode";
+
 export interface RendererOptions<HostNode = RendererNode> {
   createElement(type: string): HostNode;
 
@@ -20,10 +22,27 @@ export type RootRenderFunction<HostElement = RendererElement> = (
 ) => void;
 
 export function createRenderer(options: RendererOptions) {
-  const { setElementText: hostSetElementText } = options;
+  const {
+    setElementText: hostSetElementText,
+    createElement: hostCreateElement,
+    createText: hostCreateText,
+    insert: hostInsert,
+  } = options;
 
-  const render: RootRenderFunction = (message, container) => {
-    hostSetElementText(container, message);
+  function renderVNode(vNode: VNode | string) {
+    if (typeof vNode === "string") return hostCreateText(vNode);
+    const el = hostCreateElement(vNode.type);
+
+    for (const child of vNode.children) {
+      const childEl = renderVNode(child);
+      hostInsert(childEl, el);
+    }
+
+    return el;
+  }
+
+  const render: RootRenderFunction = (vNode, container) => {
+    hostSetElementText(container, vNode);
   }
 
   return { render };
