7c12b1ebf1e88b625239e91295582f95bd19fed4
kosuke222naito <kosuke.naito.engineer@gmail.com>
2025-01-03 11:35:59 +0900

    fix DOMRenederOptions and use PatchEvent in patchProp

--- packages/runtime-dom/patchProp.ts
+++ packages/runtime-dom/patchProp.ts
@@ -1,13 +1,14 @@
 import type { RendererOptions } from "../runtime-core";
+import { patchEvent } from "./modules/events";
 
-type DOMRenderOptions = RendererOptions
+type DOMRenderOptions = RendererOptions<Node, Element>;
 
 const onRE = /^on[^a-z]/;
 export const isOn = (key: string) => onRE.test(key);
 
 export const patchProp: DOMRenderOptions["patchProp"] = (el, key, value) => {
   if (isOn(key)) {
-    // patchEvent
+    patchEvent(el, key, value);
   } else {
     // patchAttr
   }
