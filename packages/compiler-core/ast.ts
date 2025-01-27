ebc25fe8d59b373b9da898882cd5d8b7f89ba3cc
engineer-naito <kosuke.naito.engineer@gmail.com>
2025-01-27 16:14:34 +0900

    feat: add AST node types and interfaces for template compilation

---
+++ packages/compiler-core/ast.ts
@@ -0,0 +1,38 @@
+export const enum NodeTypes {
+  ELEMENT,
+  TEXT,
+  ATTRIBUTE,
+}
+
+export interface ElementNode extends Node {
+  type: NodeTypes.ELEMENT
+  tag: string
+  props: AttributeNode[]
+  children: TemplateChildNode[]
+  isSelfClosing: boolean
+}
+
+export interface AttributeNode {
+  type: NodeTypes.ATTRIBUTE
+  name: string
+  value: TextNode | undefined
+}
+
+export type TemplateChildNode = ElementNode | TextNode
+
+export interface TextNode extends Node {
+  type: NodeTypes.TEXT
+  content: string
+}
+
+export interface SourceLocation {
+  start: Position
+  end: Position
+  source: string
+}
+
+export interface Position {
+  offset: number
+  line: number
+  column: number
+}
