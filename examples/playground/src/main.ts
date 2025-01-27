5900b9ec9f2c652a2cb552139c88812faf152629
engineer-naito <kosuke.naito.engineer@gmail.com>
2025-01-27 23:23:50 +0900

    [WIP] refactor: enhance app setup with reactive state management and update template binding

--- examples/playground/src/main.ts
+++ examples/playground/src/main.ts
@@ -1,15 +1,24 @@
-import { createApp } from "chibivue"
+import { createApp, reactive } from "chibivue"
 
 const app = createApp({
+  setup() {
+    const state = reactive({ message: "Hello chibivue!" })
+    const changeMessage = () => {
+      state.message += "!"
+    }
+  },
+
   template: `
     <div class="container" style="text-align: center">
-      <h2>Hello, chibivue!</h2>
+      <h2>{{ state.message }}</h2>
       <img
         width="150px"
         src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Vue.js_Logo_2.svg/1200px-Vue.js_Logo_2.svg.png"
         alt="Vue.js Logo"
       />
       <p><b>chibivue</b> is the minimal Vue.js</p>
+
+      <button>Click me!</button>
 
       <style>
         .container {
