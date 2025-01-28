350c9d7dffa3618388474e391ef8524962fc200d
engineer-naito <kosuke.naito.engineer@gmail.com>
2025-01-28 23:14:35 +0900

    fix: correct import name for runtime-dom in index.ts

--- packages/index.ts
+++ packages/index.ts
@@ -5,11 +5,11 @@ export * from "./reactivity"
 import { compile } from "./compiler-dom"
 import type { InternalRenderFunction } from "./runtime-core"
 import { registerRuntimeCompiler } from "./runtime-core"
-import * as runtimeDom from "./runtime-dom"
+import * as runtimeDOM from "./runtime-dom"
 
 function compileToFunction(template: string): InternalRenderFunction {
   const code = compile(template)
-  return new Function("ChibiVue", code)(runtimeDom)
+  return new Function("ChibiVue", code)(runtimeDOM)
 }
 
 registerRuntimeCompiler(compileToFunction)
