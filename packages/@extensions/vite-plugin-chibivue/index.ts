c1265072c4c6a58b83d1c35fe43d6935e449c892
engineer-naito <kosuke.naito.engineer@gmail.com>
2025-01-28 22:18:49 +0900

    feat: implement SFC parsing and template compilation in compiler-sfc

--- packages/@extensions/vite-plugin-chibivue/index.ts
+++ packages/@extensions/vite-plugin-chibivue/index.ts
@@ -1,5 +1,6 @@
 import type { Plugin } from "vite"
 import { createFilter } from "vite"
+import { parse } from "../../compiler-sfc"
 
 export default function vitePluginChibivue(): Plugin {
   const filter = createFilter(/\.vue$/)
@@ -9,6 +10,12 @@ export default function vitePluginChibivue(): Plugin {
 
     transform(code, id) {
       if (!filter(id)) return
+
+      const { descriptor } = parse(code, { filename: id })
+      console.log(
+        "🚀 ~ file: index.ts:15 ~ transform ~ description:",
+        descriptor,
+      )
       return { code: `export default {}` }
     },
   }
